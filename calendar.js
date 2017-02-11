5c830cdaec3a7237002828390aed595244ca03be
caciano <oalme004@fiu.edu>
2017-02-11 16:40:46 -0500

    added jquery ui modules

--- calendar.js
+++ calendar.js
@@ -10,25 +10,28 @@ year = date.getYear();
 var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 var firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
 
-for(var row = 1, day = 1, index = 0; row < 7; row++)
-    for(let cell = 0; cell < 7; cell++, index++)
-    {
-        if (firstDay <= index && day <= lastDay)
-        {
-            let td = document.getElementById("table").rows[row].cells[cell];
-            $(td).append(day++);
-            $(td).css("background-color", "#dcecef");
-            $(td).hover(function()
-            {
-                $(td).css("background-color", "#b1eded");
-            }, function()
-            {
+var month = new Date().getMonth();
+var months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+document.getElementById("month").innerHTML = months[month];
+
+function fillCalendar() {
+    for (var row = 1, day = 1, index = 0; row < 7; row++)
+        for (let cell = 0; cell < 7; cell++, index++) {
+            if (firstDay <= index && day <= lastDay) {
+                let td = document.getElementById("table").rows[row].cells[cell];
+                $(td).append(day++);
                 $(td).css("background-color", "#dcecef");
-            });
+                $(td).hover(function () {
+                    $(td).css("background-color", "#b1eded");
+                }, function () {
+                    $(td).css("background-color", "#dcecef");
+                });
+            }
+            else {
+                let td = document.getElementById("table").rows[row].cells[cell];
+                $(td).css("background-color", "#dbdbd6");
+            }
         }
-        else
-        {
-            let td = document.getElementById("table").rows[row].cells[cell];
-            $(td).css("background-color", "#dbdbd6");
-        }
-    }
+}
+fillCalendar();
