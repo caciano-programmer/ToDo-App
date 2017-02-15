fad1c9a4a79fdd4a6033412a775445de337c7944
caciano <oalme004@fiu.edu>
2017-02-15 13:32:57 -0500

    rearranged files

---
+++ app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Caciano on 2/15/2017.
+ */
---
+++ public/javascript/sign-up.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Caciano on 2/10/2017.
+ */
+
+function cancel()
+{
+    $("#cancel").click(function(){
+        window.location = "login.html";
+    });
+}cancel();
