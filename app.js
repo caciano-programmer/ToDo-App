d664c3434fc5900d2e4160a177c2638514cbda1e
caciano <oalme004@fiu.edu>
2017-02-17 19:43:15 -0500

    added post method to send login cred. to node server

--- app.js
+++ app.js
@@ -5,9 +5,12 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const parse = require("body-parser");
 
 app.set("view engine", "jade");
+app.use(parse.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/", function(req, res){
     res.render("login");
 });
@@ -16,6 +19,10 @@ app.get("/sign-up.html", function(req, res){
 });
 app.get("/login.html", function(req, res){
     res.render("login");
+});
+app.post("/login.html", (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
 });
 
 app.listen("3000");
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,8 +2,16 @@
  * Created by Caciano on 2/9/2017.
  */
 
-$("#new").click(function()
-{
-    window.location = "sign-up.html";
-});
+$("#new").click(function() { window.location = "sign-up.html"; });
 
+$("#login").click(()=> {
+    var email = $("#login-email").val(), password = $("#login-pass").val();
+    $.ajax(
+        {
+            type: "POST",
+            url: "/login.html",
+            data: JSON.stringify({email: email, password: password}),
+            contentType: "application/json"
+        }
+    )
+});
