33e4f292688a35896c0ac5bd696ceb02338ed762
caciano <oalme004@fiu.edu>
2017-02-19 02:14:17 -0500

    fixed routes and login info sent to server

--- app.js
+++ app.js
@@ -6,30 +6,48 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const parse = require("body-parser");
+const MongoClient = require('mongodb').MongoClient;
+const mongoURL = "mongodb://localhost:27017/calendar";
+
+function user(email, password)
+    {
+        this.email = email;
+        this.password = password
+    }
+function event(description, details, date, time)
+{
+    this.description = description;
+    this.details = details;
+    this.date = date;
+    this.time = time;
+}
 
 app.set("view engine", "jade");
 app.use(parse.json());
+app.use(parse.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+MongoClient.connect(mongoURL, (err,db) =>
+{
+
+    db.close();
+});
+
 app.get("/", function(req, res){
+    res.redirect("login.html");
+});
+app.get("/login.html", function(req, res){
     res.render("login");
 });
 app.get("/sign-up.html", function(req, res){
     res.render("sign-up");
 });
-app.get("/login.html", function(req, res){
-    res.render("login");
-});
 app.post("/login.html", (req, res) => {
-    var email = req.body.email;
-    var password = req.body.password;
+    var email = req.body.loginEmail, password = req.body.loginPass;
+    res.redirect("calendar.html");
+});
+app.get("/calendar.html", function(req, res){
+    res.render("calendar");
 });
 
 app.listen("3000");
-
-
-
-
-
-
-
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -3,15 +3,3 @@
  */
 
 $("#new").click(function() { window.location = "sign-up.html"; });
-
-$("#login").click(()=> {
-    var email = $("#login-email").val(), password = $("#login-pass").val();
-    $.ajax(
-        {
-            type: "POST",
-            url: "/login.html",
-            data: JSON.stringify({email: email, password: password}),
-            contentType: "application/json"
-        }
-    )
-});
